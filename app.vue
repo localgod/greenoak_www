e09ff6ee726acc1de9c3f0e278ad0e8d3ac8dfc8
Johannes Skov Frandsen <jsf@greenoak.dk>
2022-05-12 12:55:19 +0000

    framework setup

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script setup>
+const title = 'Greenoak';
+</script>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #717a49;
+}
+
+.container {
+  position: absolute;
+  border: 2px solid #000000;
+  left: 50%;
+  width: 800px;
+  margin-left: -400px;
+  margin-top: -250px;
+  top: 50%;
+  height: 500px;
+  background-color: #ffffff;
+}
+</style>
---
+++ dev_setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+npm install -g npm@8.10.0
+npm install
